b6807697888d3ed685e551beb3632fb7fb1f6ddc
soet02 <alswnsrlf12@naver.com>
2024-09-19 23:50:42 +0900

    html 연결

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/html/index.html');
 });
 
+app.get('/main', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html');
+});
+
+app.get('/board', (req, res) => {
+    res.sendFile(__dirname + '/html/board.html');
+});
+
+app.get('/inform', (req,res) => {
+    res.sendFile(__dirname + '/html/inform.html');
+});
+
 app.listen(port, ()=> {
     console.log('server is running at http://localhost:3000/');
 });
